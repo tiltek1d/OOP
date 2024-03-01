ce3b1792ff9c4450297e3102754b6770b163368d
tiltek1d <113026367+tiltek1d@users.noreply.github.com>
2024-03-01 12:15:36 +0300

    Initial commit

---
+++ OOP-ICT.FIrst.Tests/TestCardFunctions.cs
@@ -0,0 +1,21 @@
+﻿using OOP_ICT.Models;
+using Xunit;
+
+namespace OOP_ICT.FIrst.Tests;
+
+public class TestCardFunctions
+{
+    /// <summary>
+    /// Тесты пишутся из трех частей итог - данные - что вернуло 
+    /// </summary>
+    [Fact]
+    public void AreEquals_InputIsValueAndSuit_ReturnTrue()
+    {
+        // Пока карты не написаны ,давайте проверим числа и строки
+        var value = 10;
+        var suit = "some suit";
+        
+        Assert.Equal(10, value);
+        Assert.Equal("some suit", suit);
+    }    
+}
